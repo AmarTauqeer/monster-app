17803a421e24e311e71f8cbf0743bd603bb20c14
Amar <amar.tauqeer@gmail.com>
2020-04-19 17:57:32 +0200

    Updates

---
+++ precache-manifest.c3a32bba87252573a3d4788532a7b29e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8cfa98d0648fdde554d25a2939f54e9",
+    "url": "/monster-app/index.html"
+  },
+  {
+    "revision": "69444d1c4ba1e2fbb050",
+    "url": "/monster-app/static/css/main.72d10d4d.chunk.css"
+  },
+  {
+    "revision": "dd45d3ee5d9c24566c0c",
+    "url": "/monster-app/static/js/2.e18b89ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-app/static/js/2.e18b89ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69444d1c4ba1e2fbb050",
+    "url": "/monster-app/static/js/main.f02a787c.chunk.js"
+  },
+  {
+    "revision": "392654062798ae33c604",
+    "url": "/monster-app/static/js/runtime-main.3c149e3f.js"
+  }
+]);
